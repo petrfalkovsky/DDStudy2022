1f3e8dac1493f4735bb9a24cddef8531d5857da9
Peter Terentev <petrfalkovsky@yandex.ru>
2022-10-26 21:02:04 +0300

    feat(migraton): установлены миграции и написан контроллер

--- Api/Api/Program.cs
+++ Api/Api/Program.cs
@@ -1,16 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<DAL.DataContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSql"), sql => { });
+});
+
 var app = builder.Build();
 
+using (var serviceScope = ((IApplicationBuilder)app).ApplicationServices.GetService<IServiceScopeFactory>()?.CreateScope())
+{
+    if (serviceScope != null)
+    {
+        var context = serviceScope.ServiceProvider.GetRequiredService<DAL.DataContext>();
+        context.Database.Migrate();
+    }
+}
+
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
