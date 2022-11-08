4fceba34bdade7412c33cedba0917cca9f3418cf
Peter Terentev <petrfalkovsky@yandex.ru>
2022-11-08 11:58:40 +0300

    feat(post): created empty post controller

---
+++ Api/Controllers/PostController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        [HttpPost]
+        // todo добавить авторизацию после отладки
+        public async Task NewPost()
+        {
+
+        }
+
+    }
+}
